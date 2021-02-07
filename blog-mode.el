da40de1daf80dc8a9aa966d0c7c65ded87c688a6
suyeden <nagasuesue@yahoo.co.jp>
2021-02-07 21:46:08 +0900

    Latest version

--- blog-mode.el
+++ blog-mode.el
@@ -11,6 +11,8 @@
 (define-key org-mode-map "\C-c\C-h" 'blog-help)
 (define-key org-mode-map "\C-xx" 'all-export-to-html)
 (define-key org-mode-map "\C-cx" 'export-to-html)
+(define-key org-mode-map "\C-cs" 'blog-insert-src)
+(define-key org-mode-map "\C-c\M-s" 'blog-insert-space)
 
 ;;; blog-mode (my new major-mode)
 (defun blog-mode ()
@@ -85,14 +87,13 @@
              (string= (format "%s" f-or-d) "f")
              (string= (format "%s" f-or-d) "directory")
              (string= (format "%s" f-or-d) "d")))
-        (setq f-or-d (read-string "non-valid variable! file(f) or directory(d) ? : "))
-      nil)
+        (setq f-or-d (read-string "non-valid variable! file(f) or directory(d) ? : ")))
     (if (or (string= f-or-d "file") (string= f-or-d "f"))
         (progn
           (insert (format "[[file:%s.org][%s]]" real-name blog-name))
           (backward-char)
           (find-file (format "~/org/blog/%s.org" real-name))
-          (insert (format "#+TITLE: %s\n#+EMAIL: \n#+LANGUAGE: ja\n#+OPTIONS: toc:nil num:nil author:nil creator:nil LaTeX:t\n#+STARTUP: showall\n# file\n*  \n------------------------------------------------------------------------------------------\n** [[file:index.org][home]]\n------------------------------------------------------------------------------------------\n" blog-name))
+          (insert (format "#+TITLE: %s\n#+EMAIL: \n#+LANGUAGE: ja\n#+OPTIONS: toc:nil num:nil author:nil creator:nil LaTeX:t \\n:t\n#+STARTUP: showall\n# file\n* \n------------------------------------------------------------------------------------------\n** [[file:index.org][home]]\n------------------------------------------------------------------------------------------\n" blog-name))
           (write-file (format "~/org/blog/%s.org" real-name))
           (kill-buffer (current-buffer))
           (if (file-exists-p "~/org/blog/koushin.org")
@@ -107,14 +108,14 @@
                 (kill-buffer (current-buffer)))
             (find-file "~/org/blog/koushin.org")
             (setq now-time (format-time-string "%Y/%m/%d(%a)" (current-time)))
-            (insert (format "#+TITLE: 更新履歴\n#+EMAIL: \n#+LANGUAGE: ja\n#+OPTIONS: toc:nil num:nil author:nil creator:nil LaTeX:t\n#+STARTUP: showall\n* \n------------------------------------------------------------------------------------------\n** [[file:index.org][home]]\n------------------------------------------------------------------------------------------\n*** %s [[file:%s.org][%s]] を更新しました。" now-time real-name blog-name))
+            (insert (format "#+TITLE: 更新履歴\n#+EMAIL: \n#+LANGUAGE: ja\n#+OPTIONS: toc:nil num:nil author:nil creator:nil LaTeX:t \\n:t\n#+STARTUP: showall\n* \n------------------------------------------------------------------------------------------\n** [[file:index.org][home]]\n------------------------------------------------------------------------------------------\n*** %s [[file:%s.org][%s]] を更新しました。" now-time real-name blog-name))
             (write-file "~/org/blog/koushin.org")
             (kill-buffer (current-buffer))))
       (if (or (string= f-or-d "directory") (string= f-or-d "d"))
           (progn
             (insert (format "[[file:%s.org][%s]]" real-name blog-name))
             (find-file (format "~/org/blog/%s.org" real-name))
-            (insert (format "#+TITLE: %s\n#+EMAIL: \n#+LANGUAGE: ja\n#+OPTIONS: toc:nil num:nil author:nil creator:nil LaTeX:t\n#+STARTUP: showall\n# directory\n* \n------------------------------------------------------------------------------------------\n** [[file:index.org][home]]\n------------------------------------------------------------------------------------------\n" blog-name))
+            (insert (format "#+TITLE: %s\n#+EMAIL: \n#+LANGUAGE: ja\n#+OPTIONS: toc:nil num:nil author:nil creator:nil LaTeX:t \\n:t\n#+STARTUP: showall\n# directory\n* \n------------------------------------------------------------------------------------------\n** [[file:index.org][home]]\n------------------------------------------------------------------------------------------\n" blog-name))
             (write-file (format "~/org/blog/%s.org" real-name))
             (kill-buffer (current-buffer)))
         nil))))
@@ -122,12 +123,14 @@
 (defun export-to-html ()
   "current-buffer の orgファイル を HTMLファイル にエクスポート"
   (interactive)
+  (save-buffer)
   (execute-kbd-macro (symbol-function 'auto-export-to-html))
   (message "Done!"))
 
 (defun all-export-to-html ()
   "すべての orgファイル を HTMLファイル にエクスポートする"
   (interactive)
+  (save-buffer)
   (if (y-or-n-p "do you want to export all org-files to HTML-files? : ")
       (progn
         (let ((current-point (point)))
@@ -151,6 +154,30 @@
           (message "Done!")))
     nil))
 
+(defun blog-insert-src ()
+  "ソースコード入力の枠組み（コードブロック）を作る"
+  (interactive)
+  (let (head-spaces)
+    (setq head-spaces (current-column))
+    (insert "#+begin_src\n\n")
+    (insert (make-string head-spaces ? ))
+    (insert "#+end_src")
+    (forward-line -1)
+    (insert (make-string head-spaces ? ))))
+
+(defun blog-insert-space ()
+  "バッファ終わりまで行の先頭にスペースを入れる"
+  (interactive)
+  (let (space-count)
+    (setq space-count (read-string "how many spaces ? : "))
+    (setq space-count (string-to-number space-count))
+    (while (not (= (point) (point-max)))
+      (let ((i 0))
+        (while (not (= i space-count))
+          (insert " ")
+          (setq i (1+ i))))
+      (forward-line))))
+
 (defun end-blog ()
   "current-buffer が index.org の時に blog-mode を閉じる"
   (interactive)
@@ -169,4 +196,11 @@
 (defun blog-help ()
   "利用できるキーバインドをメッセージ表示"
   (interactive)
-  (message "C-c C-n : make a new topic (make a link)\nM-<RET> : insert a new heading\n<TAB> (next to heading) : demote a heading level\n<TAB> (on heading) : fold the current subtree up to its root level\nC-c C-o : open a topic (jump to a link destination)\nC-c <C-left> : go back to previous page\nC-c x : export current-buffer's org-file to HTML-file\nC-x x : export all org-files to HTML-files\nC-c e : close blog-mode"))
+  (get-buffer-create "*blog-help*")
+  (switch-to-buffer "*blog-help*")
+  (delete-region (point-min) (point-max))
+  (insert "\n\n\n C-c C-n : make a new topic (make a link)\n\n M-<RET> : insert a new heading\n\n <M-left> or <M-right> : change the heading level\n\n <TAB> or C-i : fold the current subtree up to its root level\n\n C-u C-i : fold all subtrees up to their root level\n\n C-c C-o : open the topic (jump to the link destination)\n\n C-c <C-left> : go back to previous page\n\n C-c s : insert a source code block\n\n C-c M-s : insert leading whitespace from current-point to end-of-buffer\n\n C-j : start a new line considering leading whitespace\n\n C-c l : store a link to the current file\n\n C-c C-l : insert a stored-link\n\n C-c x : export current-buffer's org-file to HTML-file\n\n C-x x : export all org-files to HTML-files and return to the top page\n\n C-c e : close blog-mode")
+  (while (not (string= "q" (read-string "when you quit from this page, please press 'q' and press return : ")))
+    (message "non-valid variable!")
+    (sleep-for 0.5))
+  (kill-buffer "*blog-help*"))
