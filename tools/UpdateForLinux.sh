f9ec488a6e66b1976fefcb1f14839fa567302d84
suyeden <nagasuesue@yahoo.co.jp>
2021-02-27 18:01:13 +0900

    Add .gitignore

--- tools/UpdateForLinux.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cp /home/suyeden/develop/blog-mode/blog-mode.elc /home/suyeden/.emacs.d/lisp/
--- tools/update.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-copy Z:\develop\blog-mode\blog-mode.elc Z:\.emacs.d\lisp\
-pause
